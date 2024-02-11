5e63d69541758339ef919acfc40f31ff840c1e76
matte <matte@DESKTOP-NA3JRQD>
2024-02-11 10:36:23 +0100

    Aggiungere i file di progetto.

---
+++ Vettori unari_binari/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vettori_unari_binari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vec Vettore1 = new Vec(2, 7);
+            Vec Vettore2 = new Vec(1, 9);
+            Console.WriteLine(Vettore1 + Vettore2);
+            Console.WriteLine(Vettore1 - Vettore2);
+            Console.WriteLine(+Vettore1);
+            Console.WriteLine(-Vettore1);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vettori unari_binari/Vec.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vettori_unari_binari
+{
+    internal class Vec
+    {
+        
+
+        public double  X { get; set; }
+        public double Y { get; set; }
+
+        public Vec(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public static Vec operator +(Vec v) 
+        {
+            return v;
+        }
+        public static Vec operator +(Vec v, Vec c)
+        {
+            return new Vec(v.X + c.X, v.Y + c.Y);
+        }
+        public static Vec operator -(Vec v)
+        {
+            return new Vec(-v.X, -v.Y);
+        }
+        public static Vec operator -(Vec v, Vec c) 
+        {
+            return new Vec(v.X -v.Y, v.Y -c.Y);
+        }
+        public override string ToString()
+        {
+            return string.Format("{0};{1}", X, Y);
+        }
+    }
+}
